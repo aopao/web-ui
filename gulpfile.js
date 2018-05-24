efe2e5bc9d423276ad40d391920ffa6a3ddc9805
zhuoyan <zhuoyan2015@outlook.com>
2018-05-24 19:56:37 +0800

    save work

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,13 @@ gulp.task('scss', function(){
     gulp.src('./assets/scss/*.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(gulp.dest('./assets/css'))
+    gulp.src('./assets/scss/*.css')
+        .pipe(gulp.dest('./assets/css'))
 })
 
 // 监听scss文件修改
 gulp.task('watchScss',function(){
-    gulp.watch('./assets/scss/*.scss',['scss'])
+    gulp.watch('./assets/scss/**',['scss'])
 })
 
 // 构建前先删除css文件
