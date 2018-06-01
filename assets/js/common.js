b37dea69556212a0738909da0506a28b27d01897
IBM5100 <zhuoyan2015@outlook.com>
2018-06-01 08:13:01 +0800

    nobody wanto die to fast

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-layui.use(['element','jquery'], function () {
-    var element = layui.element;
-    var $ = layui.jquery;
-    element.on('nav(side-menu)', function(elem){
-        var $items = $(this).parent();
-        $items.siblings('.layui-nav-item').removeClass('layui-nav-itemed').find('.layui-nav-item').removeClass('layui-nav-itemed');
-    });
-
-});
